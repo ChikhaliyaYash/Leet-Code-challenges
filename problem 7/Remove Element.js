efd7967573b82c725e4c3ab983133cecfa96343c
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-09-28 18:28:13 +0530

    Done

--- problem 7/Remove Element.js
+++ problem 7/Remove Element.js
@@ -6,7 +6,7 @@
  var removeElement = function(nums, val) {
     let count = 0;
     for(let i = 0 ; i < nums.length ; i++){
-        if(i < nums.length - 1 && nums[i] !== val){
+        if(i < nums.length && nums[i] !== val){ //also multiple values are here so try this >>> if(i < nums.length - 1 && nums[i] !== val)
             nums[count] = nums[i]
              count++
         }
