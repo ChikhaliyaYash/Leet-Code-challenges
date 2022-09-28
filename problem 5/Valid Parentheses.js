413df150fe5d5d60b5fabd9d5982d43b78313e06
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-09-28 18:19:56 +0530

    Done

--- problem 5/Valid Parentheses.js
+++ problem 5/Valid Parentheses.js
@@ -4,13 +4,13 @@
  */
  var isValid = function(s) {
     const leftSymbols = [];
-    // Loop for each character of the string
+   
     for (let i = 0; i < s.length; i++) {
-        // If left symbol is encountered
+       
         if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
             leftSymbols.push(s[i]);
         }
-        // If right symbol is encountered
+      
         else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '(') {
             leftSymbols.pop();
         } else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '{') {
@@ -18,7 +18,7 @@
         } else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '[') {
             leftSymbols.pop();
         }
-        // If none of the valid symbols is encountered
+       
         else {
             return false;
         }
